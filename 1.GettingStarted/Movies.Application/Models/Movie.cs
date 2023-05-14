3e9a1851efbbc652e07e120529acb13c5de2348a
vcifello <vcifello@hotmail.com>
2023-05-14 10:48:23 -0400

    Beyond Basic Crud Complete

---
+++ 1.GettingStarted/Movies.Application/Models/Movie.cs
@@ -0,0 +1,30 @@
+using System.Text.RegularExpressions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies.Application.Models;
+
+public partial class Movie
+{
+    public required Guid Id { get; init; }
+
+    public required string Title { get; set; }
+
+    public string Slug => GenerateSlug();
+
+    public required int YearOfRelease { get; set; }
+
+    public required List<string> Genres { get; init; } = new();
+
+    private string GenerateSlug()
+    {
+        var sluggedTitle = SlugRegex().Replace(Title, string.Empty)
+            .ToLower().Replace(" ", "-");
+        return $"{sluggedTitle}-{YearOfRelease}";
+    }
+
+    [GeneratedRegex("[^0-9A-Za-z _-]", RegexOptions.NonBacktracking, 5)]
+    private static partial Regex SlugRegex();
+}
---
+++ 1.GettingStarted/Movies.Application/Repositories/IMovieRepository.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Movies.Application.Models;
+
+namespace Movies.Application.Repositories;
+
+public interface IMovieRepository
+{
+    Task<bool> CreateAsync(Movie movie, CancellationToken token = default);
+
+    Task<Movie?> GetByIdAsync(Guid id, CancellationToken token = default);
+
+    Task<Movie?> GetBySlugAsync(string slug, CancellationToken token = default);
+
+    Task<IEnumerable<Movie>> GetAllAsync(CancellationToken token = default);
+
+    Task<bool> UpdateAsync(Movie movie, CancellationToken token = default);
+
+    Task<bool> DeleteByIdAsync(Guid id, CancellationToken token = default);
+
+    Task<bool> ExistsByIdAsync(Guid id, CancellationToken token = default);
+
+}
